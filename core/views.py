8f80bcd212f8ea7bcc0cda775da1b8d8c3754eac
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-28 20:39:41 +0300

    translated comments into eng

--- core/views.py
+++ core/views.py
@@ -6,25 +6,25 @@ import matplotlib.pyplot as plt
 
 class CityGrid:
     def __init__(self, n: int, m: int, obstruction_prob=0.3) -> None:
-        self.N = n  # Количество строк
-        self.M = m  # Количество столбцов
-        self.grid = [[0] * m for _ in range(n)]  # Инициализируем пустую сетку
+        self.N = n  # Number of rows
+        self.M = m  # Number of columns
+        self.grid = [[0] * m for _ in range(n)]  # Initialize an empty grid
 
-        # Размещаем преграждающие блоки случайным образом
+        # Place obstructed blocks randomly
         for row in range(n):
             for col in range(m):
                 if random.random() < obstruction_prob:
                     self.grid[row][col] = 1
 
     def print_grid(self) -> None:
-        print("\nНачальная инициализированная сетка:")
+        print("\nInitial grid:")
         for row in self.grid:
             print(" ".join(map(str, row)))
 
     def place_tower(self, row: int, col: int, tower_range: int) -> None:
-        # Проверяем, что башню можно разместить в заданных координатах
+        # Check if a tower can be placed at the given coordinates
         if 0 <= row < self.N and 0 <= col < self.M:
-            # Размещаем башню
+            # Place the tower
             for i in range(
                 max(0, row - tower_range), min(self.N, row + tower_range + 1)
             ):
@@ -32,12 +32,12 @@ class CityGrid:
                     max(0, col - tower_range), min(self.M, col + tower_range + 1)
                 ):
                     if self.grid[i][j] == 0:
-                        self.grid[i][j] = 3  # Помечаем местоположение башни
+                        self.grid[i][j] = 3  # Mark tower location
                     if self.grid[i][j] == 1:
-                        self.grid[i][j] = 2  # Помечаем зону покрытия башни
+                        self.grid[i][j] = 2  # Mark tower coverage area
 
     def display_tower_placement(self) -> None:
-        # Выводим сетку с размещенными башнями
+        # Display the grid with placed towers
         for row in self.grid:
             print(" ".join(map(str, row)))
 
@@ -48,37 +48,37 @@ class CityGrid:
         towers = []
 
         while non_obstructed_blocks:
-            # Выбираем случайный непокрытый блок
+            # Choose a random uncovered block
             block = non_obstructed_blocks[0]
             row, col = block
 
-            # Размещаем башню и обновляем список непокрытых блоков
+            # Place a tower and update the list of uncovered blocks
             self.place_tower(row, col, tower_range)
             towers.append((row, col))
             non_obstructed_blocks = [
                 (i, j) for i, j in non_obstructed_blocks if self.grid[i][j] == 0
             ]
 
-        print("\nОптимальное положение башен:")
+        print("\nOptimal tower placement:")
         self.display_tower_placement()
         return towers
 
     def visualize_city(self, tower_positions=None, data_path=None) -> None:
         fig, ax = plt.subplots()
 
-        # Визуализируем преграждающие блоки
+        # Visualize obstructed blocks
         for i in range(self.N):
             for j in range(self.M):
                 if self.grid[i][j] == 1:
                     ax.add_patch(plt.Rectangle((j, self.N - i - 1), 1, 1, color="gray"))
 
-        # Визуализируем башни
+        # Visualize towers
         if tower_positions:
             for tower in tower_positions:
                 row, col = tower
                 ax.add_patch(plt.Rectangle((col, self.N - row - 1), 1, 1, color="blue"))
 
-        # Визуализируем путь передачи данных
+        # Visualize data transmission path
         if data_path:
             for i in range(len(data_path) - 1):
                 tower1 = data_path[i]
@@ -96,9 +96,9 @@ class CityGrid:
         ax.set_ylim(0, self.N)
         plt.gca().invert_yaxis()
 
-        ax.set_title("Городская сетка с башнями и путем передачи данных")
-        ax.set_xlabel("Колонки")
-        ax.set_ylabel("Ряды")
+        ax.set_title("City grid with towers and data transmission path")
+        ax.set_xlabel("Columns")
+        ax.set_ylabel("Rows")
 
         plt.show()
 
@@ -108,7 +108,7 @@ class TowerGraph:
         self.grid = city_grid
         self.tower_range = tower_range
 
-    def find_reliable_path(self, start, end) -> list:
+    def find_reliable_path(self, start: tuple, end: tuple) -> list:
         visited = set()
         queue = deque([(start, [start])])
 
