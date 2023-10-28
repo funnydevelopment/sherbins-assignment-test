beace9110e9da43d3304768ae3353824c2d0a4b0
funnydevelopment <ya.giggs2013@yandex.ru>
2023-10-28 20:40:43 +0300

    added attempts

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def run_app():
     city.print_grid()
     tower_range = 2
     towers = city.optimize_tower_placement(tower_range)
-    print(f"\nОптимальное размещение башен: {towers}\n")
+    print(f"\nOptimal tower placement: {towers}\n")
     city.place_tower(2, 2, tower_range)
     city.place_tower(7, 7, tower_range)
 
@@ -14,13 +14,19 @@ def run_app():
     start_tower = (0, 0)
     end_tower = (9, 9)
 
-    reliable_path = tower_graph.find_reliable_path(start_tower, end_tower)
-    if reliable_path:
-        print(f"Путь с наименьшим числом переходов: {reliable_path}\n")
-    else:
-        print("Оптимального пути не найдено.")
+    reliable_path = None
+    max_attempts = 3  # Maximum number of path search attempts
 
-    # Визуализация городской сетки, башен, зон покрытия и пути передачи данных
+    for attempt in range(max_attempts):
+        reliable_path = tower_graph.find_reliable_path(start_tower, end_tower)
+        if reliable_path:
+            break
+
+    if reliable_path:
+        print(f"Path with the fewest transitions: {reliable_path}\n")
+    else:
+        print("No optimal path found after several attempts.")
+    # Visualize the city grid, towers, coverage areas, and data transmission path
     city.visualize_city(
         tower_positions=[start_tower, end_tower], data_path=reliable_path
     )
